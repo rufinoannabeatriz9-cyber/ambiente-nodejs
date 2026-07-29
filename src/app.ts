5b514ce40cd7b6265bc27a45a27c19b6e3729fc5
rufinoannabeatriz9 <rufinoannabeatriz9@gmail.com>
2026-07-29 15:07:28 -0300

    Preparação de ambiente para uso do typescript

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 console.log("Olá, TypeScript!");
 console.log("Meu nome é Anna Beatriz..");
-alert("My name is Anna Beatriz");
+console.log("Estou aprendendo TypeScript e Node.js");
+console.log("Tenho 16 anos de idade");
